d505b01241e283880a5e0510ab4fda8802a9fe9b
batlerleon14 <118742225+batlerleon14@users.noreply.github.com>
2026-04-19 18:07:27 -0400

    update all tests

--- tests/test_form_validation.py
+++ tests/test_form_validation.py
@@ -1,9 +1,13 @@
+import json
 import allure
+# загрузка данных
+with open("booking_data.json", "r") as f:
+    data = json.load(f)
 
 @allure.feature("Form")
 @allure.story("Validation")
 def test_form_empty_validation(page):
-    page.goto("https://appliancesrepairservice.ca/")
+    page.goto(data["url"])
 
     with allure.step("Submit empty form"):
         page.click('button[type="submit"]')
--- tests/test_phone.py
+++ tests/test_phone.py
@@ -1,9 +1,16 @@
+import json
 import allure
+from playwright.sync_api import expect
+from datetime import datetime, timedelta
+
+# загрузка данных
+with open("booking_data.json", "r") as f:
+    data = json.load(f)
 
 @allure.feature("Contact")
 @allure.story("Phone link")
 def test_phone_clickable(page):
-    page.goto("https://appliancesrepairservice.ca/")
+    page.goto(data["url"])
 
     with allure.step("Find phone link"):
         phone = page.locator('a[href^="tel:"]')
