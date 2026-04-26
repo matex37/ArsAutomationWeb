58ded0d8037ab69eb432036357905b3dfe38fca9
batlerleon14 <118742225+batlerleon14@users.noreply.github.com>
2026-04-25 23:17:25 -0400

    modify navigation page screenshots

--- pages/navigation_page.py
+++ pages/navigation_page.py
@@ -7,6 +7,8 @@ class NavigationPage:
 
     def open(self, url):
         self.page.goto(url)
+        self.stable_screenshot("open_page")
+
 
     def is_loaded(self):
         return self.page.locator("header").is_visible()
@@ -17,20 +19,47 @@ class NavigationPage:
     def click_services(self):
         self.page.locator("nav a.nav-link[href='/our-services/']").click()
         self.page.wait_for_load_state("networkidle")
+        self.stable_screenshot("click_services")
 
     def click_contact(self):
         self.page.get_by_role("link", name="Contact").click()
         self.page.wait_for_load_state("networkidle")
+        self.stable_screenshot("click_contact")
 
     def click_blog(self):
         blog_link = self.page.get_by_role("link", name="Blog")
         blog_link.scroll_into_view_if_needed()
         blog_link.click()
         self.page.wait_for_load_state("load")
+        self.stable_screenshot("click_blog")
 
     def click_logo(self):
         self.page.locator("img[alt*='logo'], .logo").first.click()
         self.page.wait_for_load_state("networkidle")
+        self.stable_screenshot("click_logo")
 
     def get_url(self):
         return self.page.url
+
+    def attach_screenshot(self, name):
+        screenshot = self.page.screenshot(full_page=True)
+        allure.attach(
+            screenshot,
+            name=name,
+            attachment_type=allure.attachment_type.PNG
+        )
+
+    def stable_screenshot(self, name):
+        self.page.wait_for_load_state("networkidle")
+        self.page.wait_for_timeout(500)
+
+        # подождать, пока DOM точно готов
+        self.page.wait_for_function("document.readyState === 'complete'")
+
+        allure.attach(
+            self.page.screenshot(full_page=True),
+            name=name,
+            attachment_type=allure.attachment_type.PNG
+        )
+
+
