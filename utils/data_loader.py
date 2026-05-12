b03ebd860fb46eecfab0dfbc899d6dd578cca643
batlerleon14 <118742225+batlerleon14@users.noreply.github.com>
2026-05-11 23:26:18 -0400

    update data json and data loader file with faker

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,9 +1,50 @@
 import json
 from pathlib import Path
+from faker import Faker
+
+fake = Faker("en_CA")
+
+
+def replace_faker(data):
+
+    if isinstance(data, dict):
+        return {
+            key: replace_faker(value)
+            for key, value in data.items()
+        }
+
+    elif isinstance(data, list):
+        return [
+            replace_faker(item)
+            for item in data
+        ]
+
+    elif isinstance(data, str):
+
+        faker_map = {
+            "{{first_name}}": fake.first_name(),
+            "{{last_name}}": fake.last_name(),
+            "{{email}}": fake.email(),
+            "{{street_address}}": fake.street_address(),
+            "{{phone_number}}": fake.phone_number(),
+            "{{company}}": fake.company()
+
+        }
+
+        return faker_map.get(data, data)
+
+    return data
 
 
 def load_booking_data():
-    path = Path(__file__).parent.parent /"booking_data.json"
 
-    with open(path, "r") as f:
-        return json.load(f)
+    path = (
+        Path(__file__).parent.parent
+        / "booking_data.json"
+    )
+
+    with open(path, "r", encoding="utf-8") as f:
+
+        data = json.load(f)
+
+    return replace_faker(data)
